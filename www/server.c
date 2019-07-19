5246da822c3c8ca0633499a798f7b1ce03060cbd
tessy0901 <tessy0901@gmail.com>
2019-07-19 10:32:23 +0900

    www変更

--- www/server.c
+++ www/server.c
@@ -9,6 +9,39 @@
 
 #define BUF_SIZE 256
 
+void DieWithError(char *errorMessage);
+void commun(int sock);
+int main(int argc, char *argv[]) {
+	struct sockaddr_in clientAddress;
+	unsigned int szClientAddr;
+	int cliSock;
+
+	int servSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (servSock < 0)
+        DieWithError("socket() failed");
+    
+    struct sockaddr_in servAddress;
+    servAddress.sin_family = AF_INET;
+    servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddress.sin_port = htons(80);
+
+	bind(servSock, (struct sockaddr*)&servAddress, sizeof(servAddress));
+
+	listen(servSock, 5);
+
+	while (1) {
+		szClientAddr = sizeof(clientAddress);
+		cliSock = accept(servSock, (struct sockaddr *)&clientAddress, &szClientAddr);
+
+		commun(cliSock);
+
+		close(cliSock);
+	}
+
+    close(servSock);
+    return 0;
+}
+
 void DieWithError(char *errorMessage) {
     perror(errorMessage);
     exit(1);
@@ -21,10 +54,24 @@ void commun(int sock) {
     char buf2[2*BUF_SIZE];
     int len_r;
     char response[BUF_SIZE];
+    char result[100];
+    char *uri;
+    result[0] = '\n';
+    regex_t regBuf;
+    regmatch_t regMatch[1];
+    const char *pattern = "GET[^\\n]+HTTP";
+
+    if(regcomp(&regBuf, pattern, REG_EXTENDED | REG_NEWLINE)!=0)
+        DieWithError("regcomp failed");
 
     buf_old[0] = '\0';
     
     while((len_r = recv(sock, buf, BUF_SIZE, 0)) > 0){
+        if(regexec(&regBuf, buf2, 1, regMatch,0)!=0){
+            int startIndex = regMatch[0].rm_so;
+            int endIndex = regMatch[0].rm_eo;
+            strncpy(result, buf2+startIndex, endIndex-startIndex);
+        }
         buf[len_r] = '\0';
         sprintf(buf2, "%s%s", buf_old, buf);
 
@@ -35,6 +82,14 @@ void commun(int sock) {
         }
 		
 	sprintf(buf_old, "%s", buf);
+    }
+    regfree(&regBuf);
+    if(result[0]!='\0'){
+        uri = strtok(result, " ");
+        uri = strtok(NULL, " ");
+        printf("%s\n", uri);
+    }else{
+        DieWithError("No URI");
     }
 
     if (len_r <= 0)
@@ -66,35 +121,4 @@ void commun(int sock) {
     if(send(sock, response, strlen(response), 0) != strlen(response))
         DieWithError("send() sent a message of unexpected bytes");
 
-}
-
-int main(int argc, char *argv[]) {
-	struct sockaddr_in clientAddress;
-	unsigned int szClientAddr;
-	int cliSock;
-
-	int servSock = socket(PF_INET, SOCK_STREAM, 0);
-    if (servSock < 0)
-        DieWithError("socket() failed");
-    
-    struct sockaddr_in servAddress;
-    servAddress.sin_family = AF_INET;
-    servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    servAddress.sin_port = htons(80);
-
-	bind(servSock, (struct sockaddr*)&servAddress, sizeof(servAddress));
-
-	listen(servSock, 5);
-
-	while (1) {
-		szClientAddr = sizeof(clientAddress);
-		cliSock = accept(servSock, (struct sockaddr *)&clientAddress, &szClientAddr);
-
-		commun(cliSock);
-
-		close(cliSock);
-	}
-
-    close(servSock);
-    return 0;
 }
