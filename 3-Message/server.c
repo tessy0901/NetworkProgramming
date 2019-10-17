1b8a514507a420c900a123650192afba630831fd
tessy0901 <tessy0901@gmail.com>
2019-10-17 17:01:35 +0900

    while外した

--- 3-Message/server.c
+++ 3-Message/server.c
@@ -11,6 +11,9 @@
 void DieWithError(char *);
 int prepare_server_socket(int);
 void commun(int);
+void read_until_delim(int, char *, char, int);
+int get_current_balance();
+void set_current_balance(int);
 
 int main(int argc, char *argv[]) {
 	struct sockaddr_in clientAddress;
@@ -50,4 +53,48 @@ int prepare_server_socket(int port) {
 	bind(servSock, (struct sockaddr*)&servAddress, sizeof(servAddress));
 
     return servSock;
+}
+
+void read_until_delim(int sock, char *buf, char delimiter, int max_length) {
+	int len_r = 0;
+	int index_letter = 0;
+
+	while (index_letter < max_length - 1) {
+		if ((len_r = recv(sock, buf + index_letter, 1, 0)) <= 0) {
+			printf("接続が切れました\n");
+			return;
+		}
+
+		if (buf[index_letter] == delimiter) {
+			break;
+		}else {
+			index_letter++;
+		}
+	}
+
+	buf[index_letter] = '\0';
+}
+
+int get_current_balance() {
+	return 1000000;
+}
+
+void set_current_balance(int new_balance) {
+	return;
+}
+
+void commun(int sock) {
+	char buf[BUF_SIZE];
+	int balance = get_current_balance();
+
+	read_until_delim(sock, buf, '_', BUF_SIZE);
+	balance += atoi(buf);
+	balance -= atoi(buf);
+
+	set_current_balance(balance);
+
+	sprintf(buf, "%d_", balance);
+	if(send(sock, buf, strlen(buf), 0) != strlen(buf)) {
+		DieWithError("send() sent a message of unexpected bytes");
+	}
 }
